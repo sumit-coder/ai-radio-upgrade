8e134d3c92633edbc17659136b225ce2b004c262
sumit-coder <sumitbu24@gmail.com>
2023-04-09 22:24:35 +0530

    added podcast model

---
+++ lib/model/podcast_model.dart
@@ -0,0 +1,27 @@
+class PodCast {
+  final String name;
+  final String shortDescription;
+  final String posterUrl;
+  final List<PodCastEpisode> episodes;
+
+  PodCast({
+    required this.name,
+    required this.shortDescription,
+    required this.episodes,
+    required this.posterUrl,
+  });
+}
+
+class PodCastEpisode {
+  final String title;
+  final String shortDescription;
+  final String posterUrl;
+  final String audioSourceUrl;
+
+  PodCastEpisode({
+    required this.title,
+    required this.shortDescription,
+    required this.posterUrl,
+    required this.audioSourceUrl,
+  });
+}
