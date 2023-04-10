cae7f7a37f8ae94646dcb45f7931aa4f31fa4944
govindgurjar <mr.govindbod@gmail.com>
2023-04-10 16:51:45 +0530

    changes in tag line

--- lib/utils/app_consts.dart
+++ lib/utils/app_consts.dart
@@ -1,4 +1,5 @@
 class MyConst {
   static const String appDisplayName = "Sound 🎧 Scrolls";
-  static const String appTagLineText = "Sweet as the Moment When the Sound scrolls Went 'Pop'";
+  static const String appTagLineText =
+      "Sweet As The Moment When The Sound Scrolls Went 'Pop'";
 }
