e3b1018f817af4a7bbe99b05e377d1da6e2f724c
sumit-coder <sumitbu24@gmail.com>
2023-04-12 02:53:36 +0530

    added floating podcast player

--- lib/provider/audio_player_provider.dart
+++ lib/provider/audio_player_provider.dart
@@ -4,13 +4,21 @@ import 'package:audioplayers/audioplayers.dart';
 class AudioPlayerProvider with ChangeNotifier {
   final audioPlayer = AudioPlayer();
   Duration activeAudioDuration = Duration.zero;
+  bool isPlaying = false;
+
+  setPlayPauseFunc(bool newValue) {
+    isPlaying = newValue;
+    notifyListeners();
+  }
 
   setAudioSource(String audioFileUrl) async {
     await audioPlayer.setSourceUrl(audioFileUrl);
   }
 
   playAudioPlayer(String audioFileUrl) async {
-    await audioPlayer.play(UrlSource(audioFileUrl));
+    await audioPlayer.play(UrlSource(audioFileUrl)).whenComplete(() {
+      setPlayPauseFunc(true);
+    });
     activeAudioDuration = await audioPlayer.getDuration() ?? Duration.zero;
 
     notifyListeners();
@@ -18,13 +26,16 @@ class AudioPlayerProvider with ChangeNotifier {
 
   pauseAudioPlayer() async {
     await audioPlayer.pause();
+    setPlayPauseFunc(false);
+  }
+
+  resumeAudioPlayer() async {
+    await audioPlayer.resume();
+    setPlayPauseFunc(true);
   }
 
   stopAudioPlayer(String audioFileUrl) async {
     await audioPlayer.stop();
-  }
-
-  getTimeLine() {
-    PlayerState.paused;
+    setPlayPauseFunc(false);
   }
 }
